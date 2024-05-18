d7717bf221291a8b63d5d4030abc89b186a1122b
Roman Bolshakov <rb812@yandex.ru>
2024-05-19 02:59:34 +0300

    add checkstyle 3

--- stats/stats-client/src/main/java/ru/practicum/client/StatsClient.java
+++ stats/stats-client/src/main/java/ru/practicum/client/StatsClient.java
@@ -2,28 +2,39 @@ package ru.practicum.client;
 
 import java.util.*;
 import java.util.stream.Collectors;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.*;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.DefaultUriBuilderFactory;
 import ru.practicum.EndpointHit;
 import ru.practicum.ViewStats;
 
 @Component
-@RequiredArgsConstructor
 public class StatsClient {
 
-    private final RestTemplate restTemplate = new RestTemplate();
-    private final String serverUrl = "http://stats-server:9090";
+    private final RestTemplate restTemplate;
+
+    @Autowired
+    public StatsClient(@Value("${stats-server.url}") String userServiceUrl,
+                       RestTemplateBuilder builder) {
+        this.restTemplate = builder
+                .uriTemplateHandler(new DefaultUriBuilderFactory(userServiceUrl))
+                .requestFactory(HttpComponentsClientHttpRequestFactory::new)
+                .build();
+    }
 
     public void addHit(EndpointHit endpointHit) {
-        this.restTemplate.postForEntity(serverUrl + "/hit", endpointHit, EndpointHit.class);
+        restTemplate.postForEntity("/hit", endpointHit, EndpointHit.class);
     }
 
     public ArrayList<ViewStats> getStats(String start, String end,
                                          List<String> uris, Boolean unique) {
-        String requestUrl = serverUrl + "/stats?start=" + start + "&end=" + end;
+        String requestUrl = "/stats?start=" + start + "&end=" + end;
         if (uris != null && !uris.isEmpty()) {
             String urisAsParam = "&uris=" + uris.stream()
                     .map(Object::toString)
